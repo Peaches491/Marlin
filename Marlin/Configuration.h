a9a10529b94e62c12ecb74afb1049d942e839dc3
Daniel Miller <daniel@zoox.com>
2021-06-05 22:04:18 -0700

    Fix up MAX Z settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1247,7 +1247,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 183
+#define Z_MAX_POS 181
 
 /**
  * Software Endstops
@@ -1528,7 +1528,7 @@
 
 #if ENABLED(LCD_BED_LEVELING)
   #define MESH_EDIT_Z_STEP  0.025 // (mm) Step size while manually probing Z axis.
-  #define LCD_PROBE_Z_RANGE 4     // (mm) Z Range centered on Z_MIN_POS for LCD Z adjustment
+  #define LCD_PROBE_Z_RANGE 6     // (mm) Z Range centered on Z_MIN_POS for LCD Z adjustment
   #define MESH_EDIT_MENU        // Add a menu to edit mesh points
 #endif
 
